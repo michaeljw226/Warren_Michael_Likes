ba2a06f532d57c1585df451945ae25385244ee89
michaeljw22 <michaeljwarren226@gmail.com>
2023-07-20 10:03:38 -0500

    init commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var count = 9;
+var output = document.querySelector("#output");
+function countClicks(){
+    count = count + 1;
+    output.innerHTML = count
+}
+var count2 = 12;
+var output2 = document.querySelector("#output2");
+function countClicks2(){
+    count2 = count2 + 1;
+    output2.innerHTML = count2
+}
+var count3 = 9;
+var output3 = document.querySelector("#output3");
+function countClicks3(){
+    count3 = count3 + 1;
+    output3.innerHTML = count3
+}
